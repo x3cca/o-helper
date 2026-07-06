920e1d494c62805e2d1586834b9f03c8cabbfb32
dot <dot@x3c.ca>
2026-07-06 02:05:40 -0700

    Fix O-Helper ETW overlay namespace

--- app/Overlay/EtwFpsMonitor.cs
+++ app/Overlay/EtwFpsMonitor.cs
@@ -1,7 +1,7 @@
 ﻿using System.Diagnostics;
 using System.Runtime.InteropServices;
 
-namespace GHelper.Overlay
+namespace OHelper.Overlay
 {
     public sealed class EtwFpsMonitor : IDisposable
     {
